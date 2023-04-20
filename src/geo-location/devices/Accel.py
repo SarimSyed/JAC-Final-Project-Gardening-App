56d347b145f39f2aa8daed098e33bee2ae89f5da
Braeden <braedengiasson@gmail.com>
2023-04-20 18:46:16 -0400

    Added base Buzzer class

---
+++ src/geo-location/devices/Accel.py
@@ -0,0 +1,67 @@
+import seeed_python_reterminal.core as rt
+import seeed_python_reterminal.acceleration as rt_accel
+from time import sleep
+import math
+import evdev
+
+from smbus2 import SMBus, i2c_msg
+
+# Open i2c bus 1 and read one byte from address 80, offset 0
+with SMBus(1) as bus:
+    msg = i2c_msg.read(0x19, 80)
+    bus.i2c_rdwr(msg)
+
+# import smbus
+# import time
+# import adafruit_lis3dh
+
+# # Initialize I2C bus
+# i2c = smbus.SMBus(1)
+
+# Initialize LIS3DH sensor object
+# lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c)
+
+# # Set the range of the accelerometer (optional)
+# # lis3dh.range = adafruit_lis3dh.RANGE_4_G
+
+# # Main loop
+# while True:
+#     # Read the acceleration values
+#     accel_x, accel_y, accel_z = lis3dh.acceleration
+
+#     # Print the acceleration values
+#     print("Acceleration (m/s^2): X={0:.2f}, Y={1:.2f}, Z={2:.2f}".format(accel_x, accel_y, accel_z))
+
+#     # Wait for some time before reading again
+#     time.sleep(0.1)
+
+
+# device = rt.get_acceleration_device() 
+
+
+# while True:
+    # for event in device.read_loop():
+    #     accelEvent = rt_accel.AccelerationEvent(event)
+
+    #     if accelEvent.name != None:
+    #         print(f"name={str(accelEvent.name)} value={accelEvent.value}")
+
+    #     # pitch = math.atan2(accelEvent.value)
+
+    #     print()
+
+    #     sleep(0.5)
+
+    
+
+    # g: float = 9.81
+    # roll = math.atan2(11,-1161)
+    # srqt = math.sqrt(math.pow(11, 2) + math.pow(-1161, 2))
+    # pitch = math.atan2(-(-42), srqt)
+    # vibration = math.sqrt(math.pow(-42, 2) + math.pow(11, 2) + math.pow(-1161, 2))
+
+    # print(f"roll: {roll}  |  pitch: {pitch}  |  vibration: {vibration}")
+
+    # sleep(0.5)
+
+
---
+++ src/geo-location/devices/Buzzer.py
@@ -0,0 +1,87 @@
+import seeed_python_reterminal.core as rt
+from time import sleep
+from actuators import IActuator, ACommand
+
+class Buzzer(IActuator):
+    """A Buzzer represents an alarm in the Geo-Location subsytem.
+
+    Args:
+        IActuator (IActuator): Implements the interface.
+    """
+
+    def __init__(self, type: ACommand.Type, initial_state: dict) -> None:
+        """Constructor for Buzzer class. Must define interface's class properties
+        :param ACommand.Type type: Type of command the actuator can respond to.
+        :param dict initial_state: initializes 'current_state' property of a new actuator.
+        """
+
+        # Initialize object variables
+        self.type = type or ACommand.Type.BUZZER
+        self._current_state = initial_state
+
+    def validate_command(self, command: ACommand) -> bool:
+        """Validates that a command can be used with the specific actuator.
+        :param ACommand command: the command to be validated.
+        :return bool: True if command can be consumed by the actuator.
+        """
+
+        return command.target_type == self.type and str(command.data.get("value")).lower() == "on" or str(command.data.get("value")).lower() == "off"
+
+    def control_actuator(self, data: dict) -> bool:
+        """Sets the actuator to the value passed as argument.
+        :param dict value: dictionary containing keys and values with command information.
+        :return bool: True if the state of the actuator changed, false otherwise.
+        """
+
+        # Get the previous actuator state
+        # current_state = str(self._current_state.get("value"))
+
+        # Get the value from the dictionnary
+        data_value = data["value"]
+
+        # Turn on fan
+        if data_value == "on":
+            rt.buzzer = True
+        # Turn off fan
+        elif data_value == "off":
+            rt.buzzer = False
+
+        return rt.buzzer 
+
+
+
+
+if __name__ == "__main__":
+    # while True:
+    #     rt.buzzer = True
+    #     print("BUZZER ON")
+
+    #     sleep(2)
+
+    #     rt.buzzer = False
+    #     print("BUZZER OFF")
+
+    #     sleep(2)
+
+    buzzer = Buzzer(ACommand.Type.BUZZER, {})
+
+    while True:
+        fake_fan_message_body = '{"value": "on"}'
+        fake_fan_command = ACommand(
+            ACommand.Type.BUZZER, fake_fan_message_body)
+
+        if buzzer.validate_command(fake_fan_command):
+            buzzer.control_actuator({'value': 'on'})
+
+        sleep(2)
+
+        fake_fan_message_body_off = '{"value": "off"}'
+        fake_fan_command_off = ACommand(
+            ACommand.Type.BUZZER, fake_fan_message_body_off)
+
+        if buzzer.validate_command(fake_fan_command_off):
+            buzzer.control_actuator({'value': 'off'})
+
+        sleep(2)
+    
+
