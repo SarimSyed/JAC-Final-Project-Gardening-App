aef028baed177061b3821d13de3541c509c6d0c3
Payal-Rathod <78222753+Payal-Rathod@users.noreply.github.com>
2023-04-25 14:33:50 -0400

    added maui app project

---
+++ Mobile_App/ContainerFarm/ContainerFarm/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ContainerFarm;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Mobile_App/ContainerFarm/ContainerFarm/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ContainerFarm;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
