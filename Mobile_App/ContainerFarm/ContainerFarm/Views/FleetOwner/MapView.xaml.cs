686a4a5311077a80ffa663426bdf88cd850f5e5f
Braeden <braedengiasson@gmail.com>
2023-04-27 23:30:21 -0400

    added basic map and manifest permissions for Google

--- Mobile_App/ContainerFarm/ContainerFarm/Views/FleetOwner/MapView.xaml.cs
+++ Mobile_App/ContainerFarm/ContainerFarm/Views/FleetOwner/MapView.xaml.cs
@@ -1,9 +1,14 @@
 namespace ContainerFarm.Views.FleetOwner;
+using Map = Microsoft.Maui.Controls.Maps.Map;
+
 
 public partial class MapView : ContentPage
 {
 	public MapView()
 	{
 		InitializeComponent();
-	}
+
+        Map map = new Map();
+        Content = map;
+    }
 }
