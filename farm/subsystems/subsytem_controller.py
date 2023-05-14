1bd4cac944097cf716a81b4c25fd0f34df0e66ca
SarimSyed <a.sarimsyed@gmail.com>
2023-05-14 21:48:33 +0100

    Added SubsystemController which is a class that will be used to read and control all subsystems

---
+++ farm/subsystems/subsytem_controller.py
@@ -0,0 +1,30 @@
+from subsystems.plants.PlantController import PlantSystem
+from subsystems.geoLocation.GeoLocation import GeoLocation
+from subsystems.security.Security import Security
+from interfaces.sensors import AReading
+from interfaces.actuators import ACommand
+
+
+class SubsystemController:
+    """This class will manage all the subsystems created and allow them to be manipulated from one class instead of 3 instantiations
+    """
+    def __init__(self) -> None:
+
+        self.plants : PlantSystem = PlantSystem()
+        self.security: Security = Security()
+        self.geolocation : GeoLocation = GeoLocation()
+    
+    def read_sensors(self) -> list[AReading]:
+        """Reads all subsystem sensors and returns list of all gathered data
+
+        Returns:
+            list[AReading]: List of all sensor readings
+        """
+        return self.plants.read_sensors() + self.security.read_sensors() + self.geolocation.read_sensors()
+    
+    def control_actuator(self, property )-> bool:
+        #property is the desired property that was retrieved and will be used to control actuator
+        #With an if statement make the appropriate actuator work
+
+        return True
+    
